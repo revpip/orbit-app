a1e98e4b54039e1e9d7614ecfcca8d7e449a34c3
revpip <prm@russellgrant.com>
2026-06-29 13:44:18 +0100

    Add home view

---
+++ app/Views/home/index.php
@@ -0,0 +1,9 @@
+<main>
+    <h1>Find your people.</h1>
+    <p>ORBIT is a trust-first connection platform for friendship, support, local companionship, travel, skills and meaningful real-world connection.</p>
+    <p>
+        <a href="/register">Join the beta</a>
+        &nbsp;|&nbsp;
+        <a href="/login">Sign in</a>
+    </p>
+</main>
