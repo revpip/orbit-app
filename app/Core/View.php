fcf00af8c3a46485b776699050ca3938775c366e
revpip <prm@russellgrant.com>
2026-06-29 13:40:09 +0100

    Add view renderer

---
+++ app/Core/View.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Orbit\Core;
+
+final class View
+{
+    public static function render(string $view, array $data = [], string $layout = 'app'): void
+    {
+        $viewPath = dirname(__DIR__) . '/Views/' . $view . '.php';
+        $layoutPath = dirname(__DIR__) . '/Views/layouts/' . $layout . '.php';
+
+        if (!is_file($viewPath)) {
+            http_response_code(500);
+            echo 'View not found.';
+            return;
+        }
+
+        extract($data, EXTR_SKIP);
+
+        ob_start();
+        require $viewPath;
+        $content = ob_get_clean();
+
+        if (is_file($layoutPath)) {
+            require $layoutPath;
+            return;
+        }
+
+        echo $content;
+    }
+}
